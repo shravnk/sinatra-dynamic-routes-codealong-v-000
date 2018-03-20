24c573a5ca5176fd867af5cde6f4211de52b1f19
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 21:12:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
 
-  get 'multiply/:one/:two' do
+  get '/multiply/:one/:two' do
     number1 = params[:one]
     number2 = params[:two]
     "Hi"
